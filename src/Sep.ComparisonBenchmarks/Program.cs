81c70ddfcd6a275d02aa9dd25de89dfc758430e0
ntr <ntr@ihfood.dk>
2023-08-20 11:48:55 +0200

    save bench to custom folder with name

--- src/Sep.ComparisonBenchmarks/Program.cs
+++ src/Sep.ComparisonBenchmarks/Program.cs
@@ -3,14 +3,21 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using BenchmarkDotNet.Columns;
 using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Loggers;
 using BenchmarkDotNet.Parameters;
 using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
 using nietras.SeparatedValues.ComparisonBenchmarks;
+using Perfolizer.Horology;
+
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 
 Action<string> log = t => { Console.WriteLine(t); Trace.WriteLine(t); };
@@ -20,37 +27,63 @@ log($"{Environment.Version} args: {args.Length}");
 await PackageAssetsTestData.EnsurePackageAssets().ConfigureAwait(true);
 
 // Use args as switch to run BDN or not e.g. BDN only run when using script
-if (args.Length > 0)
+if (true || args.Length > 0)
 {
-    var config = (Debugger.IsAttached ? new DebugInProcessConfig() : DefaultConfig.Instance)
+    var baseConfig = ManualConfig.CreateEmpty()
+        .AddColumnProvider(DefaultColumnProviders.Instance)
+        .AddExporter(MarkdownExporter.GitHub)
+        .AddLogger(ConsoleLogger.Default);
+
+    var config =
+#if DEBUG
+        baseConfig
+#else
+        (Debugger.IsAttached ? new DebugInProcessConfig() : DefaultConfig.Instance)
+#endif
         .WithSummaryStyle(SummaryStyle.Default.WithMaxParameterColumnWidth(120))
         .AddColumn(MB())
         .AddColumn(MBPerSec())
         .AddColumn(RowsStatisticColumn.NSPerRow())
-        .WithOption(ConfigOptions.JoinSummary, true);
+        .WithOption(ConfigOptions.JoinSummary, true)
+#if DEBUG
+        .AddJob(Job.InProcess.WithIterationTime(TimeInterval.FromMilliseconds(100)).WithMinIterationCount(2).WithMaxIterationCount(5))
+#endif
+        ;
 
     //BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args, config);
 
-    var benchTypesSet = new Type[][]
+    var nameToBenchTypesSet = new Dictionary<string, Type[]>()
     {
-        new[] { typeof(RowPackageAssetsBench), typeof(ColsPackageAssetsBench), typeof(AssetPackageAssetsBench), },
-        new[] { typeof(QuotesRowPackageAssetsBench), typeof(QuotesColsPackageAssetsBench), typeof(QuotesAssetPackageAssetsBench), },
-        new[] { typeof(RowFloatsReaderBench), typeof(ColsFloatsReaderBench), typeof(FloatsFloatsReaderBench), },
+        { nameof(PackageAssetsBench), new[] { typeof(RowPackageAssetsBench), typeof(ColsPackageAssetsBench), typeof(AssetPackageAssetsBench), } },
+        { nameof(PackageAssetsBench) + "Quotes", new[] { typeof(QuotesRowPackageAssetsBench), typeof(QuotesColsPackageAssetsBench), typeof(QuotesAssetPackageAssetsBench), } },
+        { nameof(FloatsReaderBench), new[] { typeof(RowFloatsReaderBench), typeof(ColsFloatsReaderBench), typeof(FloatsFloatsReaderBench), } },
     };
-    foreach (var benchTypes in benchTypesSet)
+    foreach (var (name, benchTypes) in nameToBenchTypesSet)
     {
         var summaries = BenchmarkRunner.Run(benchTypes, config, args);
         foreach (var s in summaries)
         {
-            log(s.ResultsDirectoryPath);
-            log(s.LogFilePath);
-            foreach (var r in s.Reports)
-            {
-                log(r.GetHardwareIntrinsicsInfo()!);
-                var metrics = r.Metrics;
-                var stats = r.ResultStatistics;
+            var cpuInfo = s.HostEnvironmentInfo.CpuInfo.Value;
+            var processorName = cpuInfo.ProcessorName;
+            var processorNameInDirectory = processorName.Replace(" Processor", "").Replace(" ", ".");
 
-            }
+            var sourceDirectory = GetSourceDirectory();
+            var directory = $"{sourceDirectory}/../../benchmarks/{processorNameInDirectory}";
+            if (!Directory.Exists(directory)) { Directory.CreateDirectory(directory); }
+            var filePath = Path.Combine(directory, $"{name}.md");
+
+            var exporter = MarkdownExporter.GitHub;
+
+            using var stream = new MemoryStream();
+            using var writer = new StreamWriter(stream);
+            using var logger = new StreamLogger(writer);
+            exporter.ExportToLog(s, logger);
+            logger.Flush();
+            stream.Position = 0;
+            using var reader = new StreamReader(stream);
+            var text = reader.ReadToEnd();
+            text = text.Replace("**", "");
+            File.WriteAllText(filePath, text);
         }
     }
 }
@@ -99,10 +132,4 @@ static long BytesFromReaderSpec(IReadOnlyList<ParameterInstance> parameters)
     return parameters.Select(p => p.Value as ReaderSpec).Where(r => r is not null).Single()!.Bytes;
 }
 
-//class Config : ManualConfig
-//{
-//    public Config()
-//    {
-//        AddJob(Job.MediumRun.WithToolchain(BenchmarkDotNet.Toolchains.InProcess.Emit.InProcessEmitToolchain.Instance).WithId("InProcess"));
-//    }
-//}
+static string GetSourceDirectory([CallerFilePath] string filePath = "") => Path.GetDirectoryName(filePath)!;

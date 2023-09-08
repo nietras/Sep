cd01964d5f9f4e8327d51fbe096ec594c8ddb546
nietras <nietras@users.noreply.github.com>
2023-09-08 12:13:18 +0200

    Fix ArgumentOutOfRangeException for stream with >int.MaxValue length (#25)
    Fix #24

--- src/Sep/SepReader.cs
+++ src/Sep/SepReader.cs
@@ -78,9 +78,15 @@ public partial class SepReader : IDisposable
             : '\0';
 
         var guessPaddingLength = 128;
-        var bufferLength = TryGetTextReaderLength(_reader, out var readerLength)
-            ? ((readerLength < CharsMinimumLength) ? (readerLength + guessPaddingLength) : CharsMinimumLength)
+        int? maybeReaderLengthEstimate = TryGetTextReaderLength(_reader, out var longReaderLength)
+            // TextReader length can be greater than int.MaxValue so have to
+            // constrain it to avoid overflow.
+            ? (int)Math.Min(longReaderLength, int.MaxValue) : null;
+        var bufferLength = maybeReaderLengthEstimate.HasValue
+            ? ((maybeReaderLengthEstimate.Value < CharsMinimumLength)
+               ? (maybeReaderLengthEstimate.Value + guessPaddingLength) : CharsMinimumLength)
             : CharsMinimumLength;
+
         _chars = ArrayPool<char>.Shared.Rent(bufferLength);
 
         var sep = options.Sep;
@@ -364,7 +370,7 @@ public partial class SepReader : IDisposable
         }
     }
 
-    static bool TryGetTextReaderLength(TextReader reader, out int length)
+    static bool TryGetTextReaderLength(TextReader reader, out long length)
     {
         // utf8 bytes can only get shorter when converted to characters so
         // if we can determine an underlying stream length, then we can
@@ -374,7 +380,7 @@ public partial class SepReader : IDisposable
             var s = sr.BaseStream;
             if (s.CanSeek)
             {
-                length = (int)s.Length;
+                length = s.Length;
                 return true;
             }
         }

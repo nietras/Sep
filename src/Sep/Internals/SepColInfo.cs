4d7f8b642776c2093e742ee172083a7fc3f3d0e6
ntr <ntr@ihfood.dk>
2023-11-10 13:53:21 +0100

    add dumb SepColInfo stuff

---
+++ src/Sep/Internals/SepColInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace nietras.SeparatedValues;
+
+record struct SepColInfo(int ColEnd, int QuoteCount);
+
+interface ISepColInfoCreate<TColInfo>
+{
+    TColInfo Create(int colEnd, int quoteCount);
+}
+
+readonly struct SepColInfoCreate : ISepColInfoCreate<SepColInfo>
+{
+    public readonly SepColInfo Create(int colEnd, int quoteCount) => new(colEnd, quoteCount);
+}
+
+readonly struct SepColEndCreate : ISepColInfoCreate<int>
+{
+    public readonly int Create(int colEnd, int quoteCount) => colEnd;
+}

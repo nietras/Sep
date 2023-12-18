7d651d71a301f51f88deb76d4af6174191b41b45
ntr <ntr@ihfood.dk>
2023-12-18 14:20:16 +0100

    expand SepToStringTest

--- src/Sep.Test/SepToStringTest.cs
+++ src/Sep.Test/SepToStringTest.cs
@@ -60,4 +60,48 @@ public class SepToStringTest
         Assert.AreNotSame(str10, str00);
         Assert.AreSame(str11, str10);
     }
+
+    [TestMethod]
+    public void SepToStringTest_PoolPerColThreadSafe()
+    {
+        var create = SepToString.PoolPerColThreadSafe();
+        var header = new SepHeader("A", new Dictionary<string, int>() { { "A", 0 } });
+        using var pool0 = create(header, colCount: 1);
+        using var pool1 = create(null, colCount: 1);
+
+        var chars = new char[] { 'a' };
+        var str00 = pool0.ToString(chars, colIndex: 0);
+        var str01 = pool0.ToString(chars, colIndex: 0);
+        var str02 = pool0.ToString(chars, colIndex: 1);
+        var str10 = pool1.ToString(chars, colIndex: 0);
+        var str11 = pool1.ToString(chars, colIndex: 0);
+
+        Assert.AreNotSame(pool1, pool0);
+        Assert.AreSame(str01, str00);
+        Assert.AreNotSame(str02, str00);
+        Assert.AreNotSame(str10, str00);
+        Assert.AreSame(str11, str10);
+    }
+
+    [TestMethod]
+    public void SepToStringTest_PoolPerColThreadSafeFixedCapacity()
+    {
+        var create = SepToString.PoolPerColThreadSafeFixedCapacity();
+        var header = new SepHeader("A", new Dictionary<string, int>() { { "A", 0 } });
+        using var pool0 = create(header, colCount: 1);
+        using var pool1 = create(null, colCount: 1);
+
+        var chars = new char[] { 'a' };
+        var str00 = pool0.ToString(chars, colIndex: 0);
+        var str01 = pool0.ToString(chars, colIndex: 0);
+        var str02 = pool0.ToString(chars, colIndex: 1);
+        var str10 = pool1.ToString(chars, colIndex: 0);
+        var str11 = pool1.ToString(chars, colIndex: 0);
+
+        Assert.AreNotSame(pool1, pool0);
+        Assert.AreSame(str01, str00);
+        Assert.AreNotSame(str02, str00);
+        Assert.AreNotSame(str10, str00);
+        Assert.AreSame(str11, str10);
+    }
 }

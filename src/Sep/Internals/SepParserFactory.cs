6dadecb2243e5915cdca5f8fe1e3d691f077ed13
nietras <nietras@users.noreply.github.com>
2025-01-23 16:07:05 +0100

    Internals: Use nameof instead of typeof in SepParserFactory to fix native AOT issue (#229)

--- src/Sep/Internals/SepParserFactory.cs
+++ src/Sep/Internals/SepParserFactory.cs
@@ -49,27 +49,27 @@ static class SepParserFactory
     {
 #if NET8_0_OR_GREATER
         if (Environment.Is64BitProcess && Avx512BW.IsSupported)
-        { Add(parsers, static sep => new SepParserAvx512PackCmpOrMoveMaskTzcnt(sep)); }
+        { Add(parsers, nameof(SepParserAvx512PackCmpOrMoveMaskTzcnt), static sep => new SepParserAvx512PackCmpOrMoveMaskTzcnt(sep)); }
         if (Environment.Is64BitProcess && (createUnaccelerated || Vector512.IsHardwareAccelerated))
-        { Add(parsers, static sep => new SepParserVector512NrwCmpExtMsbTzcnt(sep)); }
+        { Add(parsers, nameof(SepParserVector512NrwCmpExtMsbTzcnt), static sep => new SepParserVector512NrwCmpExtMsbTzcnt(sep)); }
 #endif
         if (Avx2.IsSupported)
-        { Add(parsers, static sep => new SepParserAvx2PackCmpOrMoveMaskTzcnt(sep)); }
+        { Add(parsers, nameof(SepParserAvx2PackCmpOrMoveMaskTzcnt), static sep => new SepParserAvx2PackCmpOrMoveMaskTzcnt(sep)); }
         if (Sse2.IsSupported)
-        { Add(parsers, static sep => new SepParserSse2PackCmpOrMoveMaskTzcnt(sep)); }
+        { Add(parsers, nameof(SepParserSse2PackCmpOrMoveMaskTzcnt), static sep => new SepParserSse2PackCmpOrMoveMaskTzcnt(sep)); }
         if (createUnaccelerated || Vector256.IsHardwareAccelerated)
-        { Add(parsers, static sep => new SepParserVector256NrwCmpExtMsbTzcnt(sep)); }
+        { Add(parsers, nameof(SepParserVector256NrwCmpExtMsbTzcnt), static sep => new SepParserVector256NrwCmpExtMsbTzcnt(sep)); }
         if (createUnaccelerated || Vector128.IsHardwareAccelerated)
-        { Add(parsers, static sep => new SepParserVector128NrwCmpExtMsbTzcnt(sep)); }
+        { Add(parsers, nameof(SepParserVector128NrwCmpExtMsbTzcnt), static sep => new SepParserVector128NrwCmpExtMsbTzcnt(sep)); }
         if (createUnaccelerated || Vector64.IsHardwareAccelerated)
-        { Add(parsers, static sep => new SepParserVector64NrwCmpExtMsbTzcnt(sep)); }
-        Add(parsers, static sep => new SepParserIndexOfAny(sep));
+        { Add(parsers, nameof(SepParserVector64NrwCmpExtMsbTzcnt), static sep => new SepParserVector64NrwCmpExtMsbTzcnt(sep)); }
+        Add(parsers, nameof(SepParserIndexOfAny), static sep => new SepParserIndexOfAny(sep));
     }
 
-    static void Add<TCollection, TParser>(TCollection parsers, Func<SepParserOptions, TParser> create)
+    static void Add<TCollection, TParser>(TCollection parsers, string name, Func<SepParserOptions, TParser> create)
         where TParser : ISepParser
         where TCollection : ICollection<KeyValuePair<string, Func<SepParserOptions, ISepParser>>>
     {
-        parsers.Add(new(typeof(TParser).Name, sep => create(sep)));
+        parsers.Add(new(name, sep => create(sep)));
     }
 }

275cbe679f4d622844944d95b0c25723c232944c
nietras <nietras@users.noreply.github.com>
2025-05-23 10:46:09 +0200

    fix disasm

--- disasm-tester.ps1
+++ disasm-tester.ps1
@@ -2,4 +2,4 @@ param(
     [string]$runtime = "win-arm64"
 )
 dotnet publish src/Sep.Tester/Sep.Tester.csproj -c Release -r "$runtime" -f net9.0 --self-contained true /p:PublishAot=true /p:DebugSymbols=true
-dumpbin /DISASM /SYMBOLS "artifacts\publish\Sep.Tester\release_net9.0_$runtime\Sep.Tester.exe" > "artifacts\publish\Sep.Tester\release_net9.0_$runtime\win-arm64-disassembly.asm"
+dumpbin /DISASM /SYMBOLS "artifacts\publish\Sep.Tester\release_net9.0_$runtime\Sep.Tester.exe" > "artifacts\publish\Sep.Tester\release_net9.0_$runtime\disassembly.asm"

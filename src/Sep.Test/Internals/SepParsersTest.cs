76fd2098ab5d44f4501d1b96e4f00cebcd55362c
nietras <nietras@users.noreply.github.com>
2025-06-18 14:28:28 +0200

    nit test

--- src/Sep.Test/Internals/SepParsersTest.cs
+++ src/Sep.Test/Internals/SepParsersTest.cs
@@ -14,17 +14,19 @@ public class SepParsersTest
     [TestMethod]
     public void SepParsersTest_AdvSimd_BulkMoveMask()
     {
-        var p0 = Vector128.CreateSequence<byte>(1 + 16 * 0, 1);
-        var p1 = Vector128.CreateSequence<byte>(1 + 16 * 1, 1);
-        var p2 = Vector128.CreateSequence<byte>(1 + 16 * 2, 1);
-        var p3 = Vector128.CreateSequence<byte>(1 + 16 * 3, 1);
+        byte L = 0;
+        byte H = 255;
+        var p0 = Vector128.Create(H, L, H, L, H, L, H, L, H, L, H, L, H, L, H, L);
+        var p1 = Vector128.Create(H, L, L, L, H, L, L, L, H, L, L, L, H, L, L, L);
+        var p2 = Vector128.Create(L, H, L, H, L, H, L, H, L, H, L, H, L, H, L, H);
+        var p3 = Vector128.Create(H, H, H, H, H, H, H, H, H, H, H, H, H, H, H, H);
         if (AdvSimd.Arm64.IsSupported)
         {
             var m0 = SepParserAdvSimdNrwCmpOrBulkMoveMaskTzcnt.MoveMask(p0, p1, p2, p3);
             var m1 = SepParserAdvSimdNrwCmpOrBulkMoveMaskTzcnt.MoveMask2(p0, p1, p2, p3);
             if (m0 != m1)
             {
-                Assert.Fail($"{ToBitString(m0) != ToBitString(m1)} {m0} != {m1}");
+                Assert.Fail($"{ToBitString(m0)} != {ToBitString(m1)} {m0} != {m1}");
             }
         }
         static string ToBitString(ulong value) => Convert.ToString(unchecked((long)value), 2).PadLeft(64, '0');

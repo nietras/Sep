a9a6fd8e156bd5ce9da05db405f6b44aff3471dc
nietras <nietras@users.noreply.github.com>
2025-10-21 10:29:57 +0200

    nits and readme

--- comparison-bench.ps1
+++ comparison-bench.ps1
@@ -1,4 +1,4 @@
 param(
     [string]$filter = "*"
 )
-dotnet run -c Release -f net10.0 --project src/Sep.ComparisonBenchmarks/Sep.ComparisonBenchmarks.csproj -- -m --envVars DOTNET_GCDynamicAdaptationMode:0 --warmupCount 6 --minIterationCount 5 --maxIterationCount 15 --runtimes net90 net10.0 --iterationTime 350 --hide Type Quotes Reader RatioSD Gen0 Gen1 Gen2 Error Median StdDev --filter "$filter"
+dotnet run -c Release -f net10.0 --project src/Sep.ComparisonBenchmarks/Sep.ComparisonBenchmarks.csproj -- -m --envVars DOTNET_GCDynamicAdaptationMode:0 --warmupCount 6 --minIterationCount 5 --maxIterationCount 15 --runtimes net10.0 --iterationTime 350 --hide Type Quotes Reader RatioSD Gen0 Gen1 Gen2 Error Median StdDev --filter "$filter"

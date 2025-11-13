206b5603d76b697563f2eea4e413a35a8d9a4e61
nietras <nietras@users.noreply.github.com>
2025-11-13 13:18:57 +0100

    ensure test fails in ci

--- src/Sep/SepReaderState.cs
+++ src/Sep/SepReaderState.cs
@@ -99,7 +99,7 @@ public class SepReaderState : IDisposable
     internal SepReaderState(SepReader other)
     {
         // Delegate must point to this instance's method
-        UnsafeToStringDelegate = ToStringDefault;
+        UnsafeToStringDelegate = other.UnsafeToStringDelegate; // WRONG: must be ToStringDefault;
 
         _colSpanFlags = other._colSpanFlags;
         _header = other._header;

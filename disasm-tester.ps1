6dfe0d728746d9857d13f86bce5f598b23a5fd91
nietras <nietras@users.noreply.github.com>
2025-11-14 10:23:37 +0100

    Update README.md, Native AOT publish and test (#434)

--- disasm-tester.ps1
+++ disasm-tester.ps1
@@ -1,5 +1,5 @@
 param(
     [string]$runtime = "win-arm64"
 )
-dotnet publish src/Sep.Tester/Sep.Tester.csproj -c Release -r "$runtime" -f net9.0 --self-contained true /p:PublishAot=true /p:DebugSymbols=true
-dumpbin /DISASM /SYMBOLS "artifacts\publish\Sep.Tester\release_net9.0_$runtime\Sep.Tester.exe" > "artifacts\publish\Sep.Tester\release_net9.0_$runtime\disassembly.asm"
+dotnet publish src/Sep.Tester/Sep.Tester.csproj -c Release -r "$runtime" -f net10.0 --self-contained true /p:PublishAot=true /p:DebugSymbols=true
+dumpbin /DISASM /SYMBOLS "artifacts\publish\Sep.Tester\release_net10.0_$runtime\Sep.Tester.exe" > "artifacts\publish\Sep.Tester\release_net10.0_$runtime\disassembly.asm"

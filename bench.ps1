ef93d2941b30d73fc56d50fd29670b010be3d2e5
nietras <nietras@users.noreply.github.com>
2025-12-28 11:22:44 +0100

    update bench.ps1

--- bench.ps1
+++ bench.ps1
@@ -1,1 +1,4 @@
-dotnet run -c Release -f net10.0 --project src\Sep.Benchmarks\Sep.Benchmarks.csproj -- -m --warmupCount 5 --minIterationCount 3 --maxIterationCount 9 --runtimes net80 --iterationTime 300 --filter *
+param(
+    [string]$filter = "*"
+)
+dotnet run -c Release -f net10.0 --project src\Sep.Benchmarks\Sep.Benchmarks.csproj -- -m --warmupCount 5 --minIterationCount 3 --maxIterationCount 9 --runtimes net10.0 --iterationTime 300 --filter "$filter"

e9fe7b7dd1b8b45de587032576b943f1460e5728
nietras <nietras@users.noreply.github.com>
2026-08-16 12:56:56 +0200

    Add SepWriter.Col.FormatEnum since Format<T> allocates due to enum boxing

--- src/Sep.Test/SepWriterColTest.cs
+++ src/Sep.Test/SepWriterColTest.cs
@@ -22,13 +22,15 @@ public class SepWriterColTest
     [TestMethod]
     public async ValueTask SepWriterColTest_ColIndex()
     {
-        await Run(col => Assert.AreEqual(0, col.ColIndex), null);
+        await Run(static col => Assert.AreEqual(0, col.ColIndex),
+                  expectedColValue: null, expectedAllocatedBytes: null);
     }
 
     [TestMethod]
     public async ValueTask SepWriterColTest_ColName()
     {
-        await Run(col => Assert.AreEqual(ColName, col.ColName), null);
+        await Run(static col => Assert.AreEqual(ColName, col.ColName),
+                  expectedColValue: null, expectedAllocatedBytes: null);
     }
 
     [TestMethod]
@@ -46,13 +48,15 @@ public class SepWriterColTest
     [TestMethod]
     public async ValueTask SepWriterColTest_Set_Utf8Span()
     {
-        await Run(col => col.Set(Encoding.UTF8.GetBytes(ColText)));
+        var bytes = Encoding.UTF8.GetBytes(ColText);
+        await Run(col => col.Set(bytes));
     }
 
     [TestMethod]
     public async ValueTask SepWriterColTest_Set_Utf8Span_Long()
     {
-        await Run(col => col.Set(Encoding.UTF8.GetBytes(ColTextLong)), ColTextLong);
+        var bytes = Encoding.UTF8.GetBytes(ColTextLong);
+        await Run(col => col.Set(bytes), ColTextLong);
     }
 
     [TestMethod]
@@ -92,13 +96,15 @@ public class SepWriterColTest
     [TestMethod]
     public async ValueTask SepWriterColTest_Set_InterpolatedString_F2_CultureInfoAsConfig()
     {
-        await Run(col => col.Set($"{ColValue:F2}"), ColText + ",00", CultureInfo.GetCultureInfo("da-DK"));
+        var cultureInfo = CultureInfo.GetCultureInfo("da-DK");
+        await Run(col => col.Set($"{ColValue:F2}"), ColText + ",00", cultureInfo);
     }
 
     [TestMethod]
     public async ValueTask SepWriterColTest_Set_InterpolatedString_F2_CultureInfoAsParam()
     {
-        await Run(col => col.Set(CultureInfo.GetCultureInfo("da-DK"), $"{ColValue:F2}"), ColText + ",00");
+        var cultureInfo = CultureInfo.GetCultureInfo("da-DK");
+        await Run(col => col.Set(cultureInfo, $"{ColValue:F2}"), ColText + ",00");
     }
 
     [TestMethod]
@@ -176,6 +182,30 @@ public class SepWriterColTest
         await Run(col => col.Format(f), f.Text);
     }
 
+    enum TestEnum { Aaaa = 42 }
+    [TestMethod]
+    public async ValueTask SepWriterColTest_Set_Enum()
+    {
+        await Run(col => col.Set($"{TestEnum.Aaaa}"), TestEnum.Aaaa.ToString());
+    }
+    [TestMethod]
+    public async ValueTask SepWriterColTest_Format_Enum()
+    {
+        // Unfortunately, formatting enum via T : ISpanFormatable allocates on
+        // each format, due to boxing enum. Prefer FormatEnum/Set(..) instead.
+        var expectedAllocatedBytes = System.Runtime.CompilerServices.Unsafe.SizeOf<nint>() * 3;
+        await Run(col => col.Format(TestEnum.Aaaa), TestEnum.Aaaa.ToString(),
+                  expectedAllocatedBytes: expectedAllocatedBytes);
+        await Run(col => col.Format(TestEnum.Aaaa, "D"), ((int)TestEnum.Aaaa).ToString(),
+                  expectedAllocatedBytes: expectedAllocatedBytes);
+    }
+    [TestMethod]
+    public async ValueTask SepWriterColTest_FormatEnum_Enum()
+    {
+        await Run(col => col.FormatEnum(TestEnum.Aaaa), TestEnum.Aaaa.ToString());
+        await Run(col => col.FormatEnum(TestEnum.Aaaa, "D"), ((int)TestEnum.Aaaa).ToString());
+    }
+
     public class LongSpanFormattable : ISpanFormattable
     {
         public string Text { get; } = ColTextLong;
@@ -230,7 +260,8 @@ public class SepWriterColTest
         }
     }
 
-    static async ValueTask Run(SepWriter.ColAction action, string? expectedColValue = ColText, CultureInfo? cultureInfo = null)
+    static async ValueTask Run(SepWriter.ColAction action, string? expectedColValue = ColText,
+                               CultureInfo? cultureInfo = null, long? expectedAllocatedBytes = 0)
     {
         Func<SepWriter>[] createWriters =
         [
@@ -245,7 +276,20 @@ public class SepWriterColTest
                 using var writer = createWriter();
                 {
                     using var row = writer.NewRow();
+                    // First row, first access will allocate ColImpl and supports, so warming up
                     action(row[ColName]);
+
+                    var a0 = GC.GetAllocatedBytesForCurrentThread();
+                    var col = row[ColName];
+                    var a1 = GC.GetAllocatedBytesForCurrentThread();
+                    action(col);
+                    var a2 = GC.GetAllocatedBytesForCurrentThread();
+                    // Getting col should always allocate zero bytes
+                    Assert.AreEqual(0, a1 - a0);
+                    if (expectedAllocatedBytes.HasValue)
+                    {
+                        Assert.AreEqual(expectedAllocatedBytes.Value, a2 - a1);
+                    }
                 }
                 AssertCol(expectedColValue, writer);
             }

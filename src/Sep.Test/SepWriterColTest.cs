072f19b39337784d4baaef53b4cb5b0d905f53fc
nietras <nietras@users.noreply.github.com>
2026-08-17 14:39:00 +0200

    Revise SepWriterColTest

--- src/Sep.Test/SepWriterColTest.cs
+++ src/Sep.Test/SepWriterColTest.cs
@@ -283,7 +283,7 @@ public class SepWriterColTest
     }
 
     static async ValueTask Run(SepWriter.ColAction action, string? expectedColValue = ColText,
-                               CultureInfo? cultureInfo = null, long? expectedAllocatedBytes = null)
+                               CultureInfo? cultureInfo = null, long? expectedAllocatedBytes = 0)
     {
         Func<SepWriter>[] createWriters =
         [

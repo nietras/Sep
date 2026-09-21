7ee221e3316866f2f4ebcdac8d66ad40fa2930b0
nietras <nietras@users.noreply.github.com>
2026-09-21 14:34:52 +0200

    add tfm param to scripts

--- bench.ps1
+++ bench.ps1
@@ -1,4 +1,5 @@
 param(
-    [string]$filter = "*"
+    [string]$filter = "*",
+    [string]$tfm = "net11.0"
 )
-dotnet run -c Release -f net11.0 --project src\Sep.Benchmarks\Sep.Benchmarks.csproj -- -m --warmupCount 5 --minIterationCount 3 --maxIterationCount 9 --runtimes net11.0 --iterationTime 300 --filter "$filter"
+dotnet run -c Release -f ${tfm} --project src\Sep.Benchmarks\Sep.Benchmarks.csproj -- -m --warmupCount 5 --minIterationCount 3 --maxIterationCount 9 --runtimes ${tfm} --iterationTime 300 --filter "$filter"
--- publish-tester.ps1
+++ publish-tester.ps1
@@ -1,1 +1,6 @@
-dotnet publish -r win-x64 -f net11.0 -c Release -p:PublishAot=true .\src\Sep.tester\Sep.Tester.csproj
+param(
+    [string]$runtime = "win-x64",
+    [string]$tfm = "net11.0"
+)
+dotnet publish -r ${runtime} -f ${tfm} -c Release -p:PublishAot=true .\src\Sep.tester\Sep.Tester.csproj
+dir "artifacts\publish\Sep.Tester\release_${tfm}_${runtime}\Sep.Tester.exe"

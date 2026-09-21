0a7ad5b88976b4c55da109e52e3d8f15652c22ac
nietras <nietras@users.noreply.github.com>
2026-09-21 14:39:25 +0200

    use get-childitem

--- publish-tester.ps1
+++ publish-tester.ps1
@@ -3,4 +3,4 @@ param(
     [string]$tfm = "net11.0"
 )
 dotnet publish -r ${runtime} -f ${tfm} -c Release -p:PublishAot=true .\src\Sep.tester\Sep.Tester.csproj
-dir "artifacts\publish\Sep.Tester\release_${tfm}_${runtime}\Sep.Tester.exe"
+Get-ChildItem "artifacts\publish\Sep.Tester\release_${tfm}_${runtime}\Sep.Tester.exe"
